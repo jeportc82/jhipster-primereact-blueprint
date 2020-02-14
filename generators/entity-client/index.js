38fb4bfa022cdbb9eb79538b6ea1ebb54e6a0816
Gordon Child <gordon@gordonchild.com>
2020-02-14 17:16:42 -0600

    support latest jhipster and lint tests

--- generators/entity-client/index.js
+++ generators/entity-client/index.js
@@ -2,12 +2,13 @@
 const chalk = require('chalk');
 const EntityClientGenerator = require('generator-jhipster/generators/entity-client');
 const writeFiles = require('./files').writeFiles;
+
 module.exports = class extends EntityClientGenerator {
     constructor(args, opts) {
         super(args, { fromBlueprint: true, ...opts }); // fromBlueprint variable is important
 
         if (!this.jhipsterContext) {
-            this.error(`This is a JHipster blueprint and should be used only like ${chalk.yellow('jhipster --blueprint helloworld')}`);
+            this.error(`This is a JHipster blueprint and should be used only like ${chalk.yellow('jhipster --blueprint primereact')}`);
         }
     }
 
--- test/client.spec.js
+++ test/client.spec.js
@@ -2,38 +2,49 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
+function getPreCondition() {
+    return helpers
+        .run('generator-jhipster/generators/client')
+        .withOptions({
+            'from-cli': true,
+            skipInstall: true,
+            skipServer: true,
+            blueprints: 'bluerim',
+            skipChecks: true
+        })
+        .withGenerators([
+            [
+                require('../generators/client/index.js'), // eslint-disable-line global-require
+                'jhipster-nodejs:client',
+                path.join(__dirname, '../generators/client/index.js')
+            ]
+        ]);
+}
+
+const commonPrompt = {
+    baseName: 'sampleMysql',
+    packageName: 'com.mycompany.myapp',
+    databaseType: 'sql',
+    devDatabaseType: 'h2Disk',
+    applicationType: 'monolith',
+    cacheProvider: 'ehcache',
+    authenticationType: 'jwt',
+    prodDatabaseType: 'mysql',
+    enableTranslation: true,
+    clientTheme: 'none',
+    languages: ['en'],
+    nativeLanguage: 'en',
+    buildTool: 'maven',
+    rememberMeKey: '2bb60a80889aa6e6767e9ccd8714982681152aa5'
+};
+
 describe('Subgenerator client of primereact-blueprint JHipster blueprint', () => {
     describe('Sample test', () => {
         before(done => {
-            helpers
-                .run('generator-jhipster/generators/client')
-                .withOptions({
-                    'from-cli': true,
-                    skipInstall: true,
-                    blueprint: 'primereact-blueprint',
-                    skipChecks: true
-                })
-                .withGenerators([
-                    [
-                        require('../generators/client'), // eslint-disable-line global-require
-                        'jhipster-primereact-blueprint:client',
-                        path.join(__dirname, '../generators/client/index.js')
-                    ]
-                ])
+            getPreCondition()
                 .withPrompts({
-                    baseName: 'sampleMysql',
-                    packageName: 'com.mycompany.myapp',
-                    applicationType: 'monolith',
-                    databaseType: 'sql',
-                    devDatabaseType: 'h2Disk',
-                    prodDatabaseType: 'mysql',
-                    cacheProvider: 'ehcache',
-                    authenticationType: 'session',
-                    enableTranslation: true,
-                    nativeLanguage: 'en',
-                    languages: ['fr', 'de'],
-                    buildTool: 'maven',
-                    rememberMeKey: '2bb60a80889aa6e6767e9ccd8714982681152aa5'
+                    clientFramework: 'primereact',
+                    ...commonPrompt
                 })
                 .on('end', done);
         });
